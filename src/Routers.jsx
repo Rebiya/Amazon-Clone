57f4a8ea1ad74d50a0825166e93ea33f8ab6b786
Rebiya <rebum.19@gmail.com>
2024-12-09 22:11:58 +0300

    Router is updated

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/Landing/Landing";
 import AuthPage from "./Pages/Auth/Auth";
 import OrdersPage from "./Pages/Orders/Orders";
-import PaymentPage from "./Pages/Payment/Payment";
+import Payment from "./Pages/Payment/Payment";
 import CartPage from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
-  "  pk_test_51QTVpPA3g8fQRAS3vKHatPJBiP7avo3wdXeTS417I8eXaxIzevZYFIwMZUcheDTzoEVZvdo5HgX9Go5J4OnHuigw003BMQpUEt"
+  "pk_test_51QTVpPA3g8fQRAS3vKHatPJBiP7avo3wdXeTS417I8eXaxIzevZYFIwMZUcheDTzoEVZvdo5HgX9Go5J4OnHuigw003BMQpUEt"
 );
+
 const Routers = () => {
   return (
     <Router>
@@ -23,7 +24,7 @@ const Routers = () => {
           path="/payment"
           element={
             <Elements stripe={stripePromise}>
-              <PaymentPage />
+              <Payment />
             </Elements>
           }
         />
