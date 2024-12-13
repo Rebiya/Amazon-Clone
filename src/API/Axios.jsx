b06f1027e00e4103725e5c612c9c13f37b328fc2
Rebiya <rebum.19@gmail.com>
2024-12-13 03:12:31 +0300

    Api end point is ended

--- src/API/Axios.jsx
+++ src/API/Axios.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000"
+  // baseURL: "http://localhost:4000"
+  baseURL: "https://backend-amazonapi-10.onrender.com/"
 });
 export { axiosInstance };
